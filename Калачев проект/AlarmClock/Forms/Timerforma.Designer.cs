0a16d07f123ad65dee7965b91a6a04d88d08b963
DESKTOP-28TH2T0\skmpl <skmplecounterstrike@gmail.com>
2025-04-09 09:24:22 +0300

    ЗАкончил проект

---
+++ Калачев проект/AlarmClock/Forms/Timerforma.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace AlarmClock.Forms
+{
+    partial class Timerforma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.CircleButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.Timer1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(65, 195);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStart.TabIndex = 0;
+            this.ButtonStart.Text = "Start";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(169, 195);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(115, 253);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 2;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // CircleButton
+            // 
+            this.CircleButton.Location = new System.Drawing.Point(115, 224);
+            this.CircleButton.Name = "CircleButton";
+            this.CircleButton.Size = new System.Drawing.Size(75, 23);
+            this.CircleButton.TabIndex = 3;
+            this.CircleButton.Text = "Circle";
+            this.CircleButton.UseVisualStyleBackColor = true;
+            this.CircleButton.Click += new System.EventHandler(this.CircleButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(115, 309);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Timer1
+            // 
+            this.Timer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Timer1.Location = new System.Drawing.Point(45, 18);
+            this.Timer1.Name = "Timer1";
+            this.Timer1.Size = new System.Drawing.Size(212, 61);
+            this.Timer1.TabIndex = 5;
+            this.Timer1.Text = "00:00:00";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(87, 94);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Timer2
+            // 
+            this.Timer2.Interval = 1000;
+            this.Timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // Timerforma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 346);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Timer1);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.CircleButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.ButtonStart);
+            this.Name = "Timerforma";
+            this.Text = "Секундомер";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button CircleButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label Timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer Timer2;
+    }
+}
---
+++ Калачев проект/AlarmClock/Forms/Timerforma.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmClock.Forms
+{
+    public partial class Timerforma : Form
+    {
+        private int seconds = 0;
+        public Timerforma()
+        {
+            InitializeComponent();
+            Timer2.Interval = 10;
+            UpdateTimerDisplay();
+        }
+        private void UpdateTimerDisplay()
+        {
+            TimeSpan time = TimeSpan.FromSeconds(seconds);
+            Timer1.Text = time.ToString(@"hh\:mm\:ss");
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            Timer2.Start();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            Timer2.Stop();
+            UpdateTimerDisplay();
+        }
+
+        private void CircleButton_Click(object sender, EventArgs e)
+        {
+            TimeSpan currentTime = TimeSpan.FromSeconds(seconds);
+            string lapTime = currentTime.ToString(@"hh\:mm\:ss");
+
+            // Добавляем круг в ListBox
+            listBox1.Items.Add($"Круг {listBox1.Items.Count + 1}: {lapTime}");
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            seconds = 0;
+            listBox1.Items.Clear();
+            UpdateTimerDisplay();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            UpdateTimerDisplay();
+        }
+    }
+}
